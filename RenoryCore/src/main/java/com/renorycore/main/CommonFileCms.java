e6999098e70ddf08c217958fbd5c0ac32b059b88
JPlay <kuruch2004@gmail.com>
2020-07-10 17:00:38 +0300

    ещё один коммит

--- RenoryCore/src/main/java/com/renorycore/main/CommonFileCms.java
+++ RenoryCore/src/main/java/com/renorycore/main/CommonFileCms.java
@@ -3,6 +3,8 @@ package com.renorycore.main;
 import com.renorycore.interfaces.FileCms;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,14 +16,38 @@ public class CommonFileCms extends File implements FileCms{
         super(file.getAbsolutePath());
     }
     
+    private void checkFileExistence() throws FileNotFoundException{
+        if(!super.exists()){
+            throw new FileNotFoundException("Requested file not found");
+        }
+    }
+    
+    private void checkExtensionExistence(){
+        if(super.getName().contains("[.]+")){
+            throw new RuntimeException("Extension not found in the filename");
+        }
+    }
+    
     @Override
     public String getExtension(){
+        try {
+            checkFileExistence();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        checkExtensionExistence();
         String fileName = super.getName();
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     @Override
-    public String getNameWithoutExtension() {
+    public String getNameWithoutExtension() throws FileNotFoundException {
+        try {
+            checkFileExistence();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        checkExtensionExistence();
         String fileName = super.getName();
         return fileName.substring(0, fileName.lastIndexOf("."));
     }
--- RenoryCore/src/main/java/com/renorycore/main/Main.java
+++ RenoryCore/src/main/java/com/renorycore/main/Main.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;
  */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException{
-        CommonFileCms commonFileCms = new CommonFileCms("/home/jplay/test.file");
+        CommonFileCms commonFileCms = new CommonFileCms("");
         System.out.println(commonFileCms.getExtension());
         System.out.println(commonFileCms.getNameWithoutExtension());
     }
