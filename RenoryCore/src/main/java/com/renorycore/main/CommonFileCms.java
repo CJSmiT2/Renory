0e46d1e0fd6344e976a39abcafdaaeee72efa58b
JPlay <kuruch2004@gmail.com>
2020-07-10 17:25:34 +0300

    точно не последний коммит

--- RenoryCore/src/main/java/com/renorycore/main/CommonFileCms.java
+++ RenoryCore/src/main/java/com/renorycore/main/CommonFileCms.java
@@ -3,8 +3,6 @@ package com.renorycore.main;
 import com.renorycore.interfaces.FileCms;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -12,42 +10,28 @@ import java.util.logging.Logger;
  */
 public class CommonFileCms extends File implements FileCms{
     
-    public CommonFileCms(File file){
+    public CommonFileCms(File file) throws FileNotFoundException{
         super(file.getAbsolutePath());
+        validateFile();
     }
     
-    private void checkFileExistence() throws FileNotFoundException{
+    private void validateFile() throws FileNotFoundException{
+        if(super.getName().contains("[.]+")){
+            throw new RuntimeException("Extension not found in the filename");
+        }
         if(!super.exists()){
             throw new FileNotFoundException("Requested file not found");
         }
     }
     
-    private void checkExtensionExistence(){
-        if(super.getName().contains("[.]+")){
-            throw new RuntimeException("Extension not found in the filename");
-        }
-    }
-    
     @Override
     public String getExtension(){
-        try {
-            checkFileExistence();
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        checkExtensionExistence();
         String fileName = super.getName();
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     @Override
-    public String getNameWithoutExtension() throws FileNotFoundException {
-        try {
-            checkFileExistence();
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        checkExtensionExistence();
+    public String getNameWithoutExtension(){
         String fileName = super.getName();
         return fileName.substring(0, fileName.lastIndexOf("."));
     }
