eca8a45a36786011fa1f537ff1658d9b7bbbce09
Magla Sergey <java.developer.smit@gmail.com>
2020-07-11 20:44:21 +0300

    Привел тест в рабочее состояние

--- RenoryCore/src/main/java/com/renorycore/main/CommonFileCms.java
+++ RenoryCore/src/main/java/com/renorycore/main/CommonFileCms.java
@@ -3,37 +3,42 @@ package com.renorycore.main;
 import com.renorycore.interfaces.FileCms;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author smit
  */
-public class CommonFileCms extends File implements FileCms{
-    
-    public CommonFileCms(File file) throws FileNotFoundException{
+public class CommonFileCms extends File implements FileCms {
+
+    public CommonFileCms(File file) {
         super(file.getAbsolutePath());
-        validFile();
+        try {
+            validFile();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CommonFileCms.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    
+
     @Override
-    public String getExtension(){
+    public String getExtension() {
         String fileName = super.getName();
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     @Override
-    public String getNameWithoutExtension(){
+    public String getNameWithoutExtension() {
         String fileName = super.getName();
         return fileName.substring(0, fileName.lastIndexOf("."));
     }
 
-    private void validFile() throws FileNotFoundException{
-        if(!super.exists()){
-            throw new FileNotFoundException("File '"+super.getAbsolutePath()+"' not found");
+    private void validFile() throws FileNotFoundException {
+        if (!super.exists()) {
+            throw new FileNotFoundException("File '" + super.getAbsolutePath() + "' not found");
         }
-        if(super.getName().contains("[.]+")){
-            throw new RuntimeException("Extension not found in the file '"+super.getAbsolutePath()+"'");
+        if (super.getName().contains("[.]+")) {
+            throw new RuntimeException("Extension not found in the file '" + super.getAbsolutePath() + "'");
         }
     }
 }
--- RenoryCore/src/test/java/com/renorycore/main/CommonFileCmsTest.java
+++ RenoryCore/src/test/java/com/renorycore/main/CommonFileCmsTest.java
@@ -1,6 +1,7 @@
 package com.renorycore.main;
 
 import java.io.File;
+import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -11,14 +12,15 @@ import org.junit.Test;
 public class CommonFileCmsTest {
 
     @Test
-    public void test1() {
+    public void test1() throws IOException {
         File tmpFile = new File("/tmp/my_test_file.txt");
-        
+        tmpFile.createNewFile();
+
         CommonFileCms fileCms = new CommonFileCms(tmpFile);
-        
+
         String name = fileCms.getNameWithoutExtension();
         String extension = fileCms.getExtension();
-        
+
         assertEquals("my_test_file", name);
         assertEquals("txt", extension);
     }
